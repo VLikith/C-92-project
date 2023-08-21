0898ae76634b0a347ca106af8ad48f263934806c
VLikith <97333621+VLikith@users.noreply.github.com>
2023-08-21 14:13:41 +0100

    Add files via upload

---
+++ login.js
@@ -0,0 +1,10 @@
+var player_1_name, player_2_name;
+
+function adduser()
+{
+    player_1_name=document.getElementById("player1_name_input").value;
+    player_2_name=document.getElementById("player2_name_input").value;
+    localStorage.setItem("P1_name", player_1_name);
+    localStorage.setItem("P2_name", player_2_name);
+    window.location="game_page.html";
+}
